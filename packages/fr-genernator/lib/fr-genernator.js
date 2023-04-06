51a3cf8e624bce40aad5e0b3831d79077ca486a5
Bran <1055025755@qq.com>
2023-04-06 19:23:53 +0800

    init

--- packages/fr-genernator/lib/fr-genernator.js
+++ packages/fr-genernator/lib/fr-genernator.js
@@ -5,3 +5,6 @@ module.exports = frGenernator;
 function frGenernator() {
     console.log("🚀 ~ file: fr-genernator.js:6 ~ frGenernator ~ frGenernator:")
 }
+
+const fr = require("@wywppkd/form-render")
+fr()
